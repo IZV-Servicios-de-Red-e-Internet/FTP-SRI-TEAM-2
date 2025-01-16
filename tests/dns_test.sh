587f78c864f16dfe41b0127c39eb95f4dea140f5
Ayoub El Hasnaoui <aehasnaoui12@gmail.com>
2025-01-16 13:26:16 +0100

    Feat: TEST DNS correcto

--- tests/dns_test.sh
+++ tests/dns_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-# USAGE: ./test.sh 192.168.56.103
+# USAGE: ./sol.sh 192.168.56.103
 #
 
 # Salir si algún comando falla
@@ -12,17 +12,12 @@ function resolver () {
 
 nameserver=@$1
 
-resolver mercurio.sistema.test
-resolver venus.sistema.test
-resolver tierra.sistema.test
-resolver marte.sistema.test
+resolver mercurio.sistema.sol
+resolver venus.sistema.
+resolver tierra.sistema.sol
+resolver marte.sistema.sol
 
-resolver ns1.sistema.test
-resolver ns2.sistema.test
 
-resolver sistema.test mx
-
-resolver sistema.test ns
 
 resolver -x 192.168.56.101
 resolver -x 192.168.56.102
